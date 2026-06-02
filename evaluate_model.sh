957bdcdd46d60ab0ca9323cf66917905189c73ac
Deng Tao <572480929@qq.com>
2026-06-02 15:26:58 +0800

    add evaluation configuration for evaluate_model.sh

--- evaluate_model.sh
+++ evaluate_model.sh
@@ -1,15 +1,17 @@
-export PROJECT_PATH='[Project Path Here]'
-export http_proxy="http://127.0.0.1:7890"
-export https_proxy="http://127.0.0.1:7890"
-MODEL="gpt-4-turbo"
-export STEP=50
+#vllm serve ./Qwen3.5-4B --host 0.0.0.0 --port 8000 --language-model-only --enforce-eager --reasoning-parser qwen3 --default-chat-template-kwargs '{"enable_thinking": false}'
+export PROJECT_PATH='/home/dengtao/HiAgent'
+export OPENAI_API_BASE=http://127.0.0.1:8000/v1
+export OPENAI_API_KEY=EMPTY
+MODEL="qwen3_5_4b_vllm_server"
+export STEP=30
 AGENT="ContextEfficientAgentV2"
-export EVALTASK="blocksworld"
+AGENT="VanillaAgent"
+export EVALTASK="alfworld"
 python agentboard/eval_main.py \
     --cfg-path eval_configs/hiagent/blocksworld.yaml\
-    --tasks pddl \
+    --tasks alfworld \
     --model $MODEL  \
-    --log_path ./logs/hiagent/analysis/"${AGENT}_${MODEL}_${STEP}"   \
+    --log_path ./logs/alfworld/hiagent/"smoke_test10_${AGENT}_${MODEL}_${STEP}"   \
     --project_name none \
     --baseline_dir ./data/baseline_results  \
     --max_num_steps $STEP     \
